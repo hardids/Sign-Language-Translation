46187c67679d5ff4ac28770383e1b4b92c6bae49
aqaqsubin <aqaq925120k@cnu.ac.kr>
2022-11-25 09:50:03 +0900

    admin side 모델 성능 모니터링

--- sign_lang/admin.py
+++ sign_lang/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
-from .models import ImageObject, VideoObject, Model
+from django.conf import settings
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.admin import helpers, widgets
+from .models import ImageObject, VideoObject, Model
+from django.template.response import SimpleTemplateResponse, TemplateResponse
+from django.utils.html import format_html
 
 admin.site.register(ImageObject)
 admin.site.register(VideoObject)
@@ -10,7 +13,8 @@ admin.site.register(VideoObject)
 class ModelAdmin(admin.ModelAdmin):
     list_display = ['version', 'model', 'pub_date', 'is_active']
     #list_editable = ['is_active']
-    actions = ['activate', 'deactivate', 'compare']
+    actions = ['activate', 'deactivate']
+    change_list_template = 'admin/sign_lang/model/change_list.html'
 
     def activate(self, request, queryset):
         queryset.update(is_active= True ) #queryset.update
@@ -19,22 +23,6 @@ class ModelAdmin(admin.ModelAdmin):
     def deactivate(self, request, queryset):
         queryset.update(is_active= False) #queryset.update
     deactivate.short_description = '지정 모델을 deactive 상태로 변경'
-
-    
-    def compare(self, request, queryset):
-        labels, values = [], []
-
-        for v in queryset.values('version').distinct():
-            img_qs = ImageObject.objects.filter(version=v['version'])
-            answer_qa = img_qs.filter(answer=1)
-            if img_qs.count() > 0:
-                labels += [str(v['version'])]
-                values += [100 * (answer_qa.count() / img_qs.count())]
-        
-        print(labels, values)
-        return super().changelist_view(request, extra_context={"labels_selected" : labels, "values_selected" : values})
-    compare.short_description = '지정 모델의 성능 비교'
-
     
     def changelist_view(self, request, extra_context=None):
         response = super(ModelAdmin, self).changelist_view(
@@ -46,24 +34,15 @@ class ModelAdmin(admin.ModelAdmin):
         except (AttributeError, KeyError):
             return response
 
-
-        labels, values = [], []
-        print(response.context_data)
-        
-        result_qs = qs.values('version').all()
-        print(result_qs)
-        list(map(lambda r: r.update({'check_box': admin.helpers.checkbox.render(admin.helpers.ACTION_CHECKBOX_NAME, r['version'])}), result_qs))
-        print(result_qs)
-        for v in result_qs.values('version').distinct():
+        context = []        
+        for v in qs.values('version').distinct():
             img_qs = ImageObject.objects.filter(version=v['version'])
             answer_qa = img_qs.filter(answer=1)
             if img_qs.count() > 0:
-                labels += [str(v['version'])]
-                values += [100 * (answer_qa.count() / img_qs.count())]
+                context += [{"label" : str(v['version']), "value" : 100 * (answer_qa.count() / img_qs.count())}]
         
-        response.context_data['labels'] = labels
-        response.context_data['values'] = values
-        print(labels, values)
+        response.context_data['context'] = context
+        print(context)
         return response
         
 
